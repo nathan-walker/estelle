39147a8b9f70c6209e155b00a173ff8c7114d117
Nathan Walker <me@nwalker.org>
2016-01-21 19:22:09 -0500

    Added connection creation to

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
+"use strict";
+
+var Connection = require('./Connection');
+
 module.exports = {
 	Model: require("./Model"),
-	DataTypes: require("./DataTypes")
+	DataTypes: require("./DataTypes"),
+	connect: (options) => {
+		module.exports.connection = Connection(options);
+	}
 };
